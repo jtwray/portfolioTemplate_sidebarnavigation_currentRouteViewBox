b15f06e0e6ad807055b69083d5ace49f2c672b69
James 'Tucker' Wray <tuckerwray84@gmail.com>
2023-08-30 15:25:00 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+//  sidebarOfRoutes
+// allAs
+// allBs
+
+//  currentRouteViewBox
+
+//  routes
+//  allAs
+//  allBs
+
+//  card
+
+// detailsPageA
+// detailsPageB
+
+// detailsLayout
+// Hero
+// CONTENT
+// footer
+
+// Hero
+//  header & visual Component
+
+// header
+//  title | tech stack | problem
+
+//  Content
+//   text description
+//   S T A R
+
+//  Footer
+//   viewBox footer nav
+//   links to demo and code sample
